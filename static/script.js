1a3fcb033ba481651bd5373903a26ff35cefac75
denself <denself@gmail.com>
2014-04-30 09:54:09 +0300

    Added comments to JS file

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
-var ws;
+var ws; //global WebSocket connection varieble
 var message_container
 var input_message;
-
     function createChatMsg(date, username, message) {
+        //adding new messages to message_container
         var msg = document.createElement("div");
 
         var lidate = document.createElement("i");
@@ -21,24 +21,30 @@ var input_message;
      }
 
     function openWS() {
+        //opening WebSocket connection
         ws = new WebSocket("ws://"+document.domain+":"+_port+"/chat");
         ws.onopen = function(){
+        //Invoked when a new WebSocket is opened.
             createChatMsg(Date(), "[SYSTEM]", "Connection open");
         }
         ws.onmessage = function(e) {
+        //Handle incoming messages on the WebSocket
           var data = JSON.parse(e.data);
           createChatMsg(data.date, data.username, data.message);
           message_container.scrollTop = message_container.scrollHeight;
         };
         ws.onclose = function(e) {
+        //Invoked when the WebSocket is closed.
           openWS();
         };
         ws.onerror = function(error){
+        //Invoked when the WebSocket is closed.
             console.log('Error detected: ' + error);
         }
     }
 
       function sendMessage() {
+      //Sending message to server in JSON format
         var data = { username: _username,
                      message: input_message.value};
 
@@ -49,6 +55,7 @@ var input_message;
       }
 
       window.onload = function() {
+        //initializing global variables and handlers
         message_container = document.getElementById("chat");
         input_message  = document.getElementById("message");
         input_message.addEventListener('keypress', function(e) {
@@ -58,6 +65,7 @@ var input_message;
         });
         var date = new Date()
         console.log(date);
+        //Checking if webbrowser allowing WebSockets
         if("WebSocket" in window) {
 
           createChatMsg(date, "[SYSTEM]", "WebSocket is supported by your browser!");
